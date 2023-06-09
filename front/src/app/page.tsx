7144c9b3ad9031c5563763263a061e63b31d8167
hishamac <122851090+hishamac@users.noreply.github.com>
2023-06-09 11:47:23 +0000

    saved

---
+++ front/src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import One from "@/components/1";
+import Two from "../components/2";
+import Sidebar from '@/components/Sidebar';
+
+export default function Page() {
+
+  return (
+    // <One />
+    // <Two />
+    <Sidebar />
+    // <>
+    // </>
+  );
+}
