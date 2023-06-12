e1e6352fc195c4ef098e0fff3ef2ba2b699bd095
hishamac <122851090+hishamac@users.noreply.github.com>
2023-06-12 12:01:16 +0000

    saved

--- ts-dash/src/app/page.tsx
+++ ts-dash/src/app/page.tsx
@@ -1,19 +1,16 @@
+"use client"
 import React from 'react';
 import Header from '../../components/Header';
 import TopCards from '../../components/TopCards';
 import BarChart from '../../components/BarChart';
 import RecentOrders from '../../components/RecentOrders';
 import Head from 'next/head';
+import { Sidebar } from 'flowbite-react';
 
 export default function Page({ }) {
   return (
-      <main className='bg-gray-100 min-h-screen'>
-        <Header />
-        <TopCards />
-        <div className='p-4 grid md:grid-cols-3 grid-cols-1 gap-4'>
-          <BarChart />
-          <RecentOrders />
-        </div>
-      </main>
+      <Sidebar>
+        Hi
+      </Sidebar>
   );
 }
