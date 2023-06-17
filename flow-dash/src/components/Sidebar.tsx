2896ec33e760437a756ddab7419626fba4384f4f
hishamac <122851090+hishamac@users.noreply.github.com>
2023-06-17 16:51:30 +0000

    saved

---
+++ flow-dash/src/components/Sidebar.tsx
@@ -0,0 +1,108 @@
+import { useState } from "react"
+
+export default function Sidebar() {
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+    const sidebarData = [{ name: 'Dashboard' }, { name: 'Home' }, { name: 'Products' }, { name: 'Orders' }, { name: 'Summary' }]
+    const width = sidebarOpen ? 'w-48' : 'w-16'
+    const sideText = sidebarOpen ? 'visible' : 'hidden'
+    const marginLeft = sidebarOpen ? '48' : '16'
+    const expandShrink = sidebarOpen ? <path strokeLinecap="round" strokeLinejoin="round" d="M9 9V4.5M9 9H4.5M9 9L3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5l5.25 5.25" /> : <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
+
+    return (
+        <div>
+            <aside id="separator-sidebar" className={`fixed top-0 left-0 z-40 ${width} h-screen transition-transform`} aria-label="Sidebar">
+                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+                    <a onClick={(e) => setSidebarOpen(!sidebarOpen)} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white focus:bg-slate-300 hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">{expandShrink}</svg>
+                        <span className={`flex-1 ml-3 whitespace-nowrap ${sideText}`}>shrink</span>
+                    </a>
+                    <ul className="space-y-2 font-medium">
+                        {sidebarData.map((item, index) => (
+                            <li key={index}>
+                                <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 focus:bg-slate-300 dark:hover:bg-gray-700">
+                                    <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
+                                    <span className={`flex-1 ml-3 whitespace-nowrap ${sideText}`}>{item.name}</span>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                    {/* <ul className="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
+                        <li>
+                            <a href="#" className="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+                                <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white dark:text-gray-400" focusable="false" data-prefix="fas" data-icon="gem" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M378.7 32H133.3L256 182.7L378.7 32zM512 192l-107.4-141.3L289.6 192H512zM107.4 50.67L0 192h222.4L107.4 50.67zM244.3 474.9C247.3 478.2 251.6 480 256 480s8.653-1.828 11.67-5.062L510.6 224H1.365L244.3 474.9z" /></svg>
+                                <span className="ml-4">Upgrade to Pro</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+                                <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" /><path fillRule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clipRule="evenodd" /></svg>
+                                <span className="ml-3">Documentation</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+                                <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" /></svg>
+                                <span className="ml-3">Components</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
+                                <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-2 0c0 .993-.241 1.929-.668 2.754l-1.524-1.525a3.997 3.997 0 00.078-2.183l1.562-1.562C15.802 8.249 16 9.1 16 10zm-5.165 3.913l1.58 1.58A5.98 5.98 0 0110 16a5.976 5.976 0 01-2.516-.552l1.562-1.562a4.006 4.006 0 001.789.027zm-4.677-2.796a4.002 4.002 0 01-.041-2.08l-.08.08-1.53-1.533A5.98 5.98 0 004 10c0 .954.223 1.856.619 2.657l1.54-1.54zm1.088-6.45A5.974 5.974 0 0110 4c.954 0 1.856.223 2.657.619l-1.54 1.54a4.002 4.002 0 00-2.346.033L7.246 4.668zM12 10a2 2 0 11-4 0 2 2 0 014 0z" clipRule="evenodd" /></svg>
+                                <span className="ml-3">Help</span>
+                            </a>
+                        </li>
+                    </ul> */}
+                </div>
+            </aside>
+            <div className={`p-4 ml-16 sm:ml-${marginLeft}`}>
+                <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+                    <div className="grid grid-cols-3 gap-4 mb-4">
+                        <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+                        <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                    </div>
+                    <div className="grid grid-cols-2 gap-4 mb-4">
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+                        <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                    </div>
+                    <div className="grid grid-cols-2 gap-4">
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
